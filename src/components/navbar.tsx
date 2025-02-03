a227698a7643ed24e79803f1b8184f43a51766ec
mo1nammad <mhaagrm2@gmail.com>
2025-02-04 01:09:35 +0330

    fix session bug

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,6 +10,8 @@ import HamburgerMenu from "./hamburger-menu";
 import { getSession } from "@/app/features/auth/server/actions";
 import ProfileButton from "./profile-button";
 
+export const dynamic = "force-dynamic"; // Ensure fresh data
+
 const Navbar = async () => {
   const session = await getSession();
 
