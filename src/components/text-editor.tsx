127332ba1e35174627a0fdf96be1bbfbcd5a10ec
mo1nammad <mhaagrm2@gmail.com>
2025-02-12 00:31:00 +0330

    text editor configuration

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,7 +1,31 @@
-import { cn } from "@/lib/utils";
 import React from "react";
-import LinkExt from "@tiptap/extension-link";
-import ImageExt from "@tiptap/extension-image";
+// import LinkExt from "@tiptap/extension-link";
+// import ImageExt from "@tiptap/extension-image";
+import {
+  Bold,
+  Italic,
+  Strikethrough,
+  Code,
+  List,
+  ListOrdered,
+  Quote,
+  Undo2,
+  Redo2,
+  Heading1,
+  Heading2,
+  Heading3,
+  Heading4,
+  Heading5,
+  Heading6,
+  SeparatorHorizontal,
+  Text,
+  AlignCenter,
+  AlignLeft,
+  AlignRight,
+  // PaintBucket,
+} from "lucide-react";
+
+import { cn } from "@/lib/utils";
 // src/Tiptap.tsx
 import {
   useEditor,
@@ -10,8 +34,11 @@ import {
   Editor,
 } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import TextAlign from "@tiptap/extension-text-align";
 import { Card } from "./ui/card";
+import { Toggle } from "./ui/toggle";
 
+import "./text-editor.css";
 type Props = {
   className?: string;
   toolbar?: {
@@ -25,13 +52,20 @@ type Props = {
 export default function TextEditor({ className, tipTap, toolbar }: Props) {
   const editor = useEditor({
     extensions: [
-      StarterKit,
-      LinkExt.configure({ openOnClick: true }),
-      ImageExt.configure({ inline: true }),
+      StarterKit.configure(),
+      TextAlign.configure({ types: ["paragraph", "heading"] }),
+      // LinkExt.configure({ openOnClick: true }),
+      // ImageExt.configure({ inline: true }),
     ],
+    editorProps: {
+      attributes: {
+        class:
+          "rounded-md bg-background min-h-[400px] border-input border p-3 text-left",
+      },
+    },
   });
   return (
-    <div className={cn("space-y-2.5", className)}>
+    <div className={cn("space-y-2.5 focus-visible:ring-0", className)}>
       <Toolbar {...toolbar} editor={editor} />
       <Tiptap {...tipTap} editor={editor} />
     </div>
@@ -45,11 +79,7 @@ export const Tiptap = ({
   editor,
   ...others
 }: EditorContentProps) => {
-  return (
-    <Card className={cn("border rounded-lg p-4", className)}>
-      {editor && <EditorContent editor={editor} {...others} />}
-    </Card>
-  );
+  return <EditorContent className={className} editor={editor} {...others} />;
 };
 
 type ToolbarProps = {
@@ -60,32 +90,131 @@ type ToolbarProps = {
 export const Toolbar = ({ className, editor }: ToolbarProps) => {
   if (!editor) return null;
   return (
-    <Card className={cn("", className)}>
-      <button
-        type="button"
+    <Card className={cn("text-left flex flex-wrap gap-2 p-2", className)}>
+      <Toggle
         onClick={() => editor.chain().focus().toggleBold().run()}
-        className={`px-3 py-1 rounded border ${
-          editor.isActive("bold") ? "bg-gray-300" : ""
-        }`}
+        pressed={editor.isActive("bold")}
       >
-        Bold
-      </button>
-      <button
-        type="button"
+        <Bold />
+      </Toggle>
+      <Toggle
         onClick={() => editor.chain().focus().toggleItalic().run()}
-        className={`px-3 py-1 rounded border ${
-          editor.isActive("italic") ? "bg-gray-300" : ""
-        }`}
+        pressed={editor.isActive("italic")}
       >
-        Italic
-      </button>
-      <button
-        type="button"
+        <Italic />
+      </Toggle>
+      <Toggle
+        onClick={() => editor.chain().focus().toggleStrike().run()}
+        pressed={editor.isActive("strike")}
+      >
+        <Strikethrough />
+      </Toggle>
+      <Toggle
+        onClick={() => editor.chain().focus().toggleCode().run()}
+        pressed={editor.isActive("code")}
+      >
+        <Code />
+      </Toggle>
+
+      <Toggle
+        onClick={() => editor.chain().focus().setParagraph().run()}
+        pressed={editor.isActive("paragraph")}
+      >
+        <Text />
+      </Toggle>
+      <Toggle
+        onClick={() => editor.chain().focus().setTextAlign("left").run()}
+        pressed={editor.isActive({ textAlign: "left" })}
+      >
+        <AlignLeft />
+      </Toggle>
+      <Toggle
+        onClick={() => editor.chain().focus().setTextAlign("center").run()}
+        pressed={editor.isActive({ textAlign: "center" })}
+      >
+        <AlignCenter />
+      </Toggle>
+      <Toggle
+        onClick={() => editor.chain().focus().setTextAlign("right").run()}
+        pressed={editor.isActive({ textAlign: "right" })}
+      >
+        <AlignRight />
+      </Toggle>
+      <Toggle
+        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+        pressed={editor.isActive("heading", { level: 1 })}
+      >
+        <Heading1 />
+      </Toggle>
+      <Toggle
+        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+        pressed={editor.isActive("heading", { level: 2 })}
+      >
+        <Heading2 />
+      </Toggle>
+      <Toggle
+        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+        pressed={editor.isActive("heading", { level: 3 })}
+      >
+        <Heading3 />
+      </Toggle>
+      <Toggle
+        onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+        pressed={editor.isActive("heading", { level: 4 })}
+      >
+        <Heading4 />
+      </Toggle>
+      <Toggle
+        onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
+        pressed={editor.isActive("heading", { level: 5 })}
+      >
+        <Heading5 />
+      </Toggle>
+      <Toggle
+        onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
+        pressed={editor.isActive("heading", { level: 6 })}
+      >
+        <Heading6 />
+      </Toggle>
+      <Toggle
         onClick={() => editor.chain().focus().toggleBulletList().run()}
-        className="px-3 py-1 rounded border"
+        pressed={editor.isActive("bulletList")}
       >
-        Bullet List
-      </button>
+        <List />
+      </Toggle>
+      <Toggle
+        onClick={() => editor.chain().focus().toggleOrderedList().run()}
+        pressed={editor.isActive("orderedList")}
+      >
+        <ListOrdered />
+      </Toggle>
+      <Toggle
+        onClick={() => editor.chain().focus().toggleBlockquote().run()}
+        pressed={editor.isActive("blockquote")}
+      >
+        <Quote />
+      </Toggle>
+      <Toggle onClick={() => editor.chain().focus().setHorizontalRule().run()}>
+        <SeparatorHorizontal />
+      </Toggle>
+      <Toggle
+        onClick={() => editor.chain().focus().undo().run()}
+        disabled={!editor.can().chain().focus().undo().run()}
+      >
+        <Undo2 />
+      </Toggle>
+      <Toggle
+        onClick={() => editor.chain().focus().redo().run()}
+        disabled={!editor.can().chain().focus().redo().run()}
+      >
+        <Redo2 />
+      </Toggle>
+      {/* <Toggle
+        onClick={() => editor.chain().focus().setColor("#958DF1").run()}
+        pressed={editor.isActive("textStyle", { color: "#958DF1" })}
+      >
+        <PaintBucket />
+      </Toggle> */}
     </Card>
   );
 };
