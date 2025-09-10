cf7322a27560f9cf474f537332caaea502ef8b85
mo1nammad <mhaagrm2@gmail.com>
2025-09-10 16:54:10 +0330

    finish search page

--- src/app/(navigation)/guide/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function GuidePage() {
-  return <div>big hello</div>;
-}
--- src/app/features/shop/search/components/product-grid.tsx
+++ src/app/features/shop/search/components/product-grid.tsx
@@ -1,16 +1,45 @@
 "use client";
 
 import React, { useState } from "react";
+
 import { useSearchProducts } from "../api/use-search-products";
-import ProductCard from "$/shop/components/product-card";
-import SmartPagination from "@/components/smart-pagination";
 import { useFilterQuery } from "../hooks/use-filter-query";
+
+import SmartPagination from "@/components/smart-pagination";
+import ProductCard from "$/shop/components/product-card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Ghost } from "lucide-react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function ProductGrid() {
   const { data, status } = useSearchProducts();
   const [pagination, setPagination] = useState(data?.page ?? 1);
 
   useFilterQuery({ Page: pagination });
+
+  if (status === "error")
+    return (
+      <div className="w-full h-1/2 flex flex-col gap-1.5 items-center justify-center">
+        <Ghost className="size-8 text-muted-foreground" />
+        <h6 className="text-lg text-muted-foreground">مشکلی پیش آمد</h6>
+        <Link href="/shop/search">
+          <Button size="sm">بازگشت</Button>
+        </Link>
+      </div>
+    );
+
+  if (status === "pending")
+    return (
+      <div className="w-full grid gap-7 md:grid-cols-2 lg:grid-cols-3 grid-flow-row">
+        <Skeleton className="w-full h-29 md:h-83" />
+        <Skeleton className="w-full h-29 md:h-83" />
+        <Skeleton className="w-full h-29 md:h-83" />
+        <Skeleton className="w-full h-29 md:h-83" />
+        <Skeleton className="w-full h-29 md:h-83" />
+        <Skeleton className="w-full h-29 md:h-83 hidden md:block" />
+      </div>
+    );
 
   if (status === "success")
     return (
